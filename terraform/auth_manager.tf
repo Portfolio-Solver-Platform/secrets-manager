14c8d57e8cc20951b3c9216e5c9c3778acb9a615
Jonas Bork <jonas10bork@gmail.com>
2026-04-01 12:05:22 +0200

    Add the bootstrap service credentials for the auth manager

--- terraform/auth_manager.tf
+++ terraform/auth_manager.tf
@@ -1,4 +1,23 @@
 
+resource "vault_kubernetes_auth_backend_role" "auth_manager_role" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "auth-manager-role"
+  bound_service_account_names      = ["keycloak-operator"]
+  bound_service_account_namespaces = ["keycloak"]
+  token_policies                   = [vault_policy.auth_manager_bootstrap.name, vault_policy.auth_manager_bootstrap_service.name]
+  token_ttl                        = 3600
+}
+
+resource "vault_kubernetes_auth_backend_role" "auth_manager_tofu_role" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "auth-manager-tofu-role"
+  bound_service_account_names      = ["tofu-runner-auth-manager"]
+  bound_service_account_namespaces = ["flux-system"]
+  token_policies                   = [vault_policy.auth_manager_bootstrap_service.name]
+  token_ttl                        = 3600
+}
+
+# Bootstrap admin
 ephemeral "random_password" "auth_manager_bootstrap_admin" {
   length  = 24
   special = false
@@ -15,15 +34,6 @@ resource "vault_kv_secret_v2" "auth_manager_bootstrap_admin_credentials" {
   data_json_wo_version = 1
 }
 
-resource "vault_kubernetes_auth_backend_role" "auth_manager_role" {
-  backend                          = vault_auth_backend.kubernetes.path
-  role_name                        = "auth-manager-role"
-  bound_service_account_names      = ["keycloak-operator"]
-  bound_service_account_namespaces = ["keycloak"]
-  token_policies                   = [vault_policy.auth_manager_bootstrap.name]
-  token_ttl                        = 3600
-}
-
 data "vault_policy_document" "auth_manager_bootstrap_rules" {
   rule {
     path         = "kv/data/auth-manager/bootstrap-admin-credentials"
@@ -35,3 +45,33 @@ resource "vault_policy" "auth_manager_bootstrap" {
   name   = "auth-manager-bootstrap-policy"
   policy = data.vault_policy_document.auth_manager_bootstrap_rules.hcl
 }
+
+# Bootstrap service
+ephemeral "random_password" "auth_manager_bootstrap_service" {
+  length  = 64
+  special = false
+}
+
+resource "vault_kv_secret_v2" "auth_manager_bootstrap_service_credentials" {
+  mount = local.mounts.kv.path
+  name  = "auth-manager/bootstrap-service-credentials"
+
+  data_json_wo = jsonencode({
+    username = "tofu-runner"
+    password = ephemeral.random_password.auth_manager_bootstrap_service.result
+  })
+  data_json_wo_version = 1
+}
+
+data "vault_policy_document" "auth_manager_bootstrap_service_rules" {
+  rule {
+    path         = "kv/data/auth-manager/bootstrap-service-credentials"
+    capabilities = ["read"]
+  }
+}
+
+resource "vault_policy" "auth_manager_bootstrap_service" {
+  name   = "auth-manager-bootstrap-service-policy"
+  policy = data.vault_policy_document.auth_manager_bootstrap_service_rules.hcl
+}
+
